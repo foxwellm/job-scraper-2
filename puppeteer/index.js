ba7f9af95f91590d759950723c1a4eabb33b4f48
Matthew Foxwell <foxwellm@gmail.com>
2023-05-11 09:43:47 -0700

    refactor: puppeteer scrape compact

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -2,80 +2,63 @@ const puppeteer = require("puppeteer");
 const { addJobs } = require("../storage");
 const { findAllRemovedTitles } = require("../storage");
 
-// remote https://www.glassdoor.com/Job/remote-software-engineer-jobs-SRCH_IL.0,6_IS11047_KO7,24.htm?fromAge=3&minRating=4.00
-
-// https://www.glassdoor.com/Job/everett-software-engineer-jobs-SRCH_IL.0,7_IC1150458_KO8,25.htm?fromAge=3&minRating=4.00&radius=100
-// https://www.glassdoor.com/Job/everett-software-developer-jobs-SRCH_IL.0,7_IC1150458_KO8,26.htm?fromAge=3&minRating=4.00&radius=100
-
+// @TODO: fromAge argument
 const initialPages = [
   "https://www.glassdoor.com/Job/remote-software-engineer-jobs-SRCH_IL.0,6_IS11047_KO7,24.htm?fromAge=1&minRating=4.00",
   "https://www.glassdoor.com/Job/seattle-software-engineer-jobs-SRCH_IL.0,7_IC1150458_KO8,25.htm?fromAge=1&minRating=4.00&radius=100",
+  // "https://www.glassdoor.com/Job/everett-software-developer-jobs-SRCH_IL.0,7_IC1150458_KO8,26.htm?fromAge=1&minRating=4.00&radius=100"
 ];
 
 (async () => {
-  const browser = await puppeteer.launch();
-  // const browser = await puppeteer.launch({ headless: false });
-
+  // @TODO: headless argument
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: {
+      width: 1920,
+      height: 880,
+      isMobile: false,
+      isLandscape: true,
+    },
+    // args: ["--desktop-window-1080p"],
+  });
   const page = await browser.newPage();
   // Avoid site detecting headless browser
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
   );
 
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-
   const scrapeRunTime = Date.now();
 
   for (let i = 0; i < initialPages.length; i++) {
     const scrapePage = initialPages[i];
 
-    await page.goto(scrapePage, { waitUntil: "networkidle0" }); // TODO: while loop
+    await page.goto(scrapePage, { waitUntil: "networkidle0" });
     console.log(`Successfully navigated to ${scrapePage}`);
-    await page.waitForSelector(".jobDescriptionContent", { visible: true });
-    await page.waitForSelector(".paginationFooter");
 
     let paginationNumber = 1;
 
     while (paginationNumber !== 0) {
-      console.log(`Scraping jobs from page ${paginationNumber}`);
-
       try {
-        // skip first page
-        if (paginationNumber !== 1) {
-          const sel = await page.waitForSelector(
-            `[data-test="pagination-link-${paginationNumber}"]`,
-            {
-              timeout: 3000,
-            }
-          );
+        await page.waitForSelector(".jobDescriptionContent", { visible: true });
+        await page.waitForSelector(".paginationFooter");
+        await page.$(".react-job-listing > div > a");
+        console.log(`Scraping jobs from page ${paginationNumber}`);
 
-          await sel.click();
-          console.log("clicked");
-          await new Promise((r) => setTimeout(r, 5000));
-          console.log(`Successfully navigated to page ${paginationNumber}`);
-
-          try {
-            const desc = await page.$(".paginationFooter");
-            const { pF } = await page.evaluate((el) => {
-              return {
-                pF: el.innerText,
-              };
-            }, desc);
-            console.log(
-              "🚀 ~ file: index.js:69 ~ const{pF}=awaitpage.evaluate ~ pF:",
-              pF
-            );
-          } catch (err) {
-            console.log("🚀 ~ file: index.js:77 ~ err:", err);
-          }
-        }
         const newJobs = await scrapePageJobs(page, scrapeRunTime, scrapePage);
         addJobs(newJobs);
         console.log(`Successfully added new jobs`);
         paginationNumber++;
+
+        const sel = await page.waitForSelector(
+          `[data-test="pagination-link-${paginationNumber}"]`,
+          {
+            timeout: 3000,
+          }
+        );
+
+        await sel.click();
       } catch (err) {
-        console.log("Next page not found, setting pagination to 0");
+        console.error(err);
         paginationNumber = 0;
       }
     }
@@ -89,20 +72,48 @@ async function scrapePageJobs(page, scrapeRunTime, scrapePage) {
   const elements = await page.$$(".react-job-listing");
 
   for (let i = 0; i < elements.length; i++) {
-    const { title, location, jobCode, gdId, href, isEasyApply, companyName } =
-      await page.evaluate((el) => {
-        return {
-          title: el.lastElementChild.childNodes[1]?.innerText,
-          location: el["dataset"].jobLoc,
-          jobCode: el["dataset"].sgocId,
-          gdId: el["dataset"].id,
-          href: el.firstElementChild.firstElementChild?.href,
-          companyName:
-            el.lastElementChild.firstElementChild.firstElementChild?.innerText,
-          isEasyApply: el["dataset"].isEasyApply,
-        };
-      }, elements[i]);
-    console.log("🚀 ~ file: index.js:80 ~ scrapePageJobs ~ title:", title);
+    const {
+      title,
+      location,
+      jobCode,
+      gdId,
+      href,
+      isEasyApply,
+      companyName,
+    } = await page.evaluate((el) => {
+      const location = el["dataset"].jobLoc;
+      const jobCode = el["dataset"].sgocId;
+      const gdId = el["dataset"].id;
+      const isEasyApply = el["dataset"].isEasyApply;
+
+      let title = el.lastElementChild.childNodes[1]?.innerText;
+      let href, companyName;
+
+      // NOTE: Sometimes the page will render in "compact" styling
+      if (title) {
+        href = el.firstElementChild.firstElementChild?.href;
+        companyName =
+          el.lastElementChild.firstElementChild.firstElementChild?.innerText;
+      } else {
+        title =
+          el.firstElementChild.firstElementChild.firstElementChild
+            .firstElementChild.childNodes[1]?.innerText;
+        href = el.firstElementChild.firstElementChild.firstElementChild?.href;
+        companyName =
+          el.firstElementChild.firstElementChild.firstElementChild
+            .firstElementChild.firstElementChild.lastElementChild.innerText;
+      }
+
+      return {
+        title,
+        location,
+        jobCode,
+        gdId,
+        href,
+        companyName,
+        isEasyApply,
+      };
+    }, elements[i]);
 
     if (!title || !href) {
       console.log("no title");
@@ -120,17 +131,20 @@ async function scrapePageJobs(page, scrapeRunTime, scrapePage) {
     }
 
     // DESCRIPTION
-    // await element.click();
-    // await page.waitForNetworkIdle();
+    await elements[i].click();
+    await page.waitForNetworkIdle();
 
-    // const desc = await page.$(".jobDescriptionContent");
-    // const { jobDesc } = await page.evaluate((el) => {
-    //   return {
-    //     jobDesc: el.innerText,
-    //   };
-    // }, desc);
-    // console.log("🚀 ~ file: index.js:107 ~ const{jobDesc}=awaitpage.evaluate ~ jobDesc:", jobDesc.length)
-    // console.log("🚀 ~ file: index.js:107 ~ scrapePageJobs ~ title:", title)
+    const desc = await page.$(".jobDescriptionContent");
+    const { jobDesc } = await page.evaluate((el) => {
+      return {
+        jobDesc: el.innerText,
+      };
+    }, desc);
+
+    if (!checkDescriptionContains(jobDesc)) {
+      console.log(`Not adding ${href}`);
+      continue;
+    }
 
     let scrapeLocation = "";
     if (scrapePage.includes("remote")) {
@@ -168,4 +182,12 @@ function checkTitleContainsRemovedWord(title) {
       return myRegex.test(title.toLowerCase());
     })
   );
+}
+
+function checkDescriptionContains(jobDesc) {
+  const checkWords = ["node", "javascript", "react", "vue"];
+
+  return checkWords.some((checkWord) => {
+    return jobDesc.toLowerCase().includes(checkWord);
+  });
 }
